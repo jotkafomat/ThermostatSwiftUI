73b0ce71ef0d8f279839b0dcfd2799571c0686e3
jotkafomat <k-j@xl.wp.pl>
2020-11-05 21:23:13 +0000

    Initial Commit

---
+++ Thermostat/ThermostatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThermostatApp.swift
+//  Thermostat
+//
+//  Created by Krzysztof Jankowski on 05/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ThermostatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
